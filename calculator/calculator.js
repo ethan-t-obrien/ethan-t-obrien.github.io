c2b216f51b6c6c18cd380fd45ae386ea89e6aac3
Ethan OBrien <ethanobrien1009@gmail.com>
2019-10-09 10:36:06 +1300

    Cloned master calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,165 @@
+// create arr var to store entries of numbers
+
+// create var total, this is the answer
+
+//create var temp set to empty ''.
+
+//make button on click run function, var val = button text hint $(this).text() ?? research further
+//run if function. if val is not a number OR val is === to '.'
+//make temp + and = val;
+//get html element answer/input box and make #answer extract from the temp string from 0 - 10
+
+//create else if function for AC so that it clears everything, entries, temp, total, and answer to extract empty val
+//create another else if function for CE that clears the last entry only. so clear temp and answer to empty val again
+
+//create an else if function for symbols other than euqals,
+//add current temp(number) to entries
+//add symbol math property to entries 
+//clear temp 
+//repeat for all symbols, except + and - :)
+
+//create else if for for = button.
+//add temp to entries
+//create new var nt make nt equal to number of 1st entries
+//run for loop with initilaizer starting at 1 not zero, then run through entries length with i++
+//create var nextNum which is equal to next item in entires, hint (Number(entries[i +1])
+//create var symbol which is equal to entries[i]
+
+//create if function symbol true equal to +, function nt += nextNum
+//create else if, symbol === -, nt -=nextNum
+//create else if, symbol === *, nt *= nextnum
+//create else if, symbol === /, nt /= nextNum
+// create incrementor of i++
+
+//we need to swap the '-' so the text input handles it correctly
+//create if statement, nt is less the 0
+//make nt equal to Math.abs(nt) + '-'  FunFact :) (this takes nt absolute value and then ads the - to it so the answer shows as negative)
+
+//make answer val nt
+//clear entries
+//clear temp
+
+//create else statement 
+//add temp to entries
+//add val to entries
+//clear temp
+
+var entries = []
+var total = 0
+var temp = ''
+
+
+function getNum (numClicked) {
+  // alert(numClicked)
+  var val = numClicked 
+  console.log(val)
+}
+
+// document.getElementsByTagName('button').onclick = function() {
+//   var val = this.text
+//   alert(val)
+// }
+// document.getElementById("CE").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("%").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("÷").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+
+
+// document.getElementById("7").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("8").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("9").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("x").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+
+
+// document.getElementById("4").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("5").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("6").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("-").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+
+
+// document.getElementById("1").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("2").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("3").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("+").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+
+
+// document.getElementById("0").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById(".").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+// document.getElementById("=").onclick = function() {
+//   var val = this.value
+//   alert(val)
+// }
+
+// $("button").click(function() {
+//   var val = $(this).text()
+//   alert(val)
+// } 
+
+
+
+
+ 
+     
+   
+ 
+
+
+  
+
+  
+
+
+
+
+  
+
